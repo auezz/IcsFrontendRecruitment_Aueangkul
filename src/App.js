28272f44561a4e213c0bb989ea9fe934036ad167
auezz <dorayaki.sk2.we301@gmail.com>
2022-05-11 17:04:08 +0700

    11-05-22_2

--- src/App.js
+++ src/App.js
@@ -10,31 +10,106 @@ function App() {
     const [pageSelect, setPageSelect] = useState(1);
     const [itemLength, setItemLength] = useState(null);
     const [searchText, setSearchText] = useState(null);
+    const [listAll, setListAll] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [resId, setResid] = useState(null);
+    const [placeAPI, setPlaceAPI] = useState([]);
+    const [width, setWidth]   = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+    const updateDimensions = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    /*
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    */
+
+    useEffect(()=>{
+        const getListAll = async () =>{
+            const data = await fetchListAll();
+            console.log('listAll: ', data);
+            setListAll(data);
+        }
+        
+        getListAll();
+    },[]);
 
     useEffect(()=>{
         const getRestaurant = async () =>{
-            const data = await fetchRestaurant({pageSelect, searchText});
+            const data = await fetchRestaurant({pageSelect, searchText, resId});
             setRestaurant(data);
             console.log('data: ', data);
         }
         getRestaurant();
-    },[pageSelect, searchText]);
+    },[pageSelect, searchText, resId]);
 
-    const fetchRestaurant = async ({pageSelect, searchText})=>{
-        const res = await Axios.get(`http://localhost:5000/restaurant?_page=${pageSelect}&_limit=9&_sort=id&_order=asc`);
+    const fetchRestaurant = async ({pageSelect, searchText, resId})=>{
+        const resIdText = resId!==""?`&${resId}`:"";
+        const res = await Axios.get(`http://localhost:5000/restaurant?_page=${pageSelect}${resIdText}&_limit=9&_sort=id&_order=asc`);
         const data = await res.data;
         setItemLength(res.headers["x-total-count"]);
         //setTotalGreetingList(res.headers["x-total-count"]);
         return data;
+    }
 
+    const fetchListAll = async()=>{
+        const res = await Axios.get(`http://localhost:5000/restaurant`);
+        const data = await res.data;
+        return data;
     }
 
     const onPageChange = (page)=>{
+        if(page===null){
+            return;
+        }
         setPageSelect(page);
     }
 
-    const onSearch = (text)=>{
+    const onSelectChange = (selectId)=>{
+        console.log('selectId: ', selectId);
+        const selectVal = selectId.target.value;
+        console.log('selectVal: ', selectVal);
+        if(selectVal!==0){
+            console.log('checked');
+            let list = [];
+            listAll.forEach((item)=>{
+                //console.log('item: ', item,'\n',item.categories.includes(selectVal));
+                if(item.categories.includes(selectVal)===true){
+                    list.push(item.id);
+                }
+            });
+            //const listId = listAll.map(item=>item.categories.find(elm=>elm===selectVal));
+            //const listId = listAll.find(item=>item.categories.includes(selectVal));
+            //console.log('listId: ', list, list.join('&id='));
+            setResid(`&id=${list.join('&id=')}`);
 
+            
+        }
+
+    }
+
+    const onSearch = (text, selectVal)=>{
+        //setSearchText(text);
+        
+        if(text.length>0){
+            console.log(text, selectVal);
+            let list = [];
+            let textLower = text.toLowerCase();
+            listAll.forEach((item)=>{
+                let resName = item.name.toLowerCase();
+                if(item.categories.includes(selectVal)===true && resName.includes(textLower)===true){
+                    list.push(item.id);
+                }
+            });
+            if(list.length>0){
+                setResid(`&id=${list.join('&id=')}`);
+            }
+            //console.log('list: ', list);
+        }else{
+            setResid(null);
+        }
     }
 
     
@@ -44,14 +119,18 @@ function App() {
     return (
         <div className="App">
             <Sidebar/>
-            <Header/> 
+            <Header/>
+            {screen.width>500?
             <PageContent restaurant={restaurant} 
-                        itemLength={itemLength}
-                        currentPage={pageSelect}
-                        onPageChange={onPageChange}
-                        >
+                itemLength={itemLength}
+                currentPage={pageSelect}
+                onPageChange={onPageChange}
+                onSelectChange={onSelectChange}
+                onSearch={onSearch}
+            />:null
+            } 
+            
 
-            </PageContent>
             
 
         </div>
--- src/component/PageContent.js
+++ src/component/PageContent.js
@@ -4,12 +4,10 @@ import styled from 'styled-components';
 import { FaSearch, FaCalendar, FaCircle, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 export default function PageContent(props) {
-    const {restaurant, itemLength, currentPage, onPageChange} = props;
+    const { restaurant, itemLength, currentPage, onPageChange, onSelectChange, onSearch } = props;
 
-    const resOpenTime = (operation_time)=>{
+    const resOpenTime = (operation_time) => {
         console.log('hello');
-
-
     }
 
     const CircleIcon = styled(FaCircle)`
@@ -30,23 +28,34 @@ export default function PageContent(props) {
         color: #9E9E9E;
     `;
 
+    const onInputSearch = (e)=>{
+        const text = e.target.value;
+        const selectElem = document.getElementById('selectResType');
+        const selectVal = selectElem.value;
+        console.log(text, selectVal);
+        onSearch(text, selectVal);
+    }
 
-    const Pagination = ()=>{
-        const pageAmount = Math.round(itemLength/9);
+
+    const Pagination = () => {
+        const pageAmount = Math.round(itemLength / 9);
         console.log('pageAmount: ', pageAmount);
         return (
-                <div className='pagination d-flex gap-3 mt-5'>
-                    <div className="pagePrev"
-                         onClick={()=>{
-                             onPageChange(currentPage-1);
-                         }}   
-                    ><PrevIcon/></div>
-                    <div className="pagePrev">{currentPage}</div>
-                    <div className="pageNext" onClick={()=>{
-                        onPageChange(currentPage+1);
-                    }}><NextIcon/></div>
+            <div className='pagination d-flex gap-3 mt-5'>
+                <div className={`pagePrev ${currentPage>1?"":"page-disabled"}`}
+                    onClick={() => {
+                        onPageChange(currentPage>1?currentPage-1:null);
+                    }}
+                ><PrevIcon /></div>
+                <div className="pagePrev">{currentPage}</div>
+                <div className={`pageNext ${currentPage!==pageAmount?"":"page-disabled"}`}
+                    onClick={() => {
+                        onPageChange(currentPage!==pageAmount?currentPage+1:null);
+                    }}>
+                    <NextIcon />
                 </div>
-                )
+            </div>
+        )
     }
 
 
@@ -56,59 +65,70 @@ export default function PageContent(props) {
                 <div className="page-head d-flex">
                     <div className="page-title">Place List</div>
                     <div className="page-input w-50 d-flex gap-5">
-                        <select className="form-select" aria-label="Default select example">
+                        <select className="form-select" aria-label="Default select example"
+                            id='selectResType'
+                            onChange={(e)=>{
+                                onSelectChange(e);
+                            }}
+                        >
                             <option value={0}>Open this select menu</option>
-                            <option value="1">Restaurant</option>
-                            <option value="2">Bakery</option>
-                            <option value="3">Cafe</option>
+                            <option value="restaurant">Restaurant</option>
+                            <option value="bakery">Bakery</option>
+                            <option value="cafe">Cafe</option>
                         </select>
                         <div className="input-group flex-nowrap">
                             <input type="text"
                                 className="form-control"
-                                placeholder="Search name..." aria-label="Username" aria-describedby="addon-wrapping" />
+                                placeholder="Search name..." aria-label="Username" aria-describedby="addon-wrapping" 
+                                onChange={(e)=>{
+                                    onInputSearch(e);
+                                }}
+                                />
                             <span className="input-group-text" id="addon-wrapping"><FaSearch /></span>
                         </div>
                     </div>
                 </div>
                 <div className="restaurant-content row">
                     {
-                        restaurant.map((item,index)=>(
+                        restaurant.map((item, index) => (
+                            //(Number.isInteger((index+1)/3)?<div className='row'>:"";
                             <div className='card col-4'>
                                 <div className="card-head d-flex gap-3">
                                     <img className='w-25' src={item.profile_image_url} alt="" />
                                     <div className="res-info w-75">
                                         <h6 className='res-name'>{item.name}</h6>
-                                        <div className="d-flex gap-3">
+                                        <div className="res-subinfo d-flex gap-3">
                                             <p className="res-time d-flex gap-2 mb-0">
-                                                <FaCalendar/>
-                                                {   ((item) => {
-                                                        const getTime = item.operation_time.find(item=>item.time_open!=="closed" && item.time_close!=="closed");
-                                                        return<div>{getTime.time_open} AM - {getTime.time_close} PM</div>
-                                                    })(item)
+                                                <FaCalendar />
+                                                {((item) => {
+                                                    const getTime = item.operation_time.find(item => item.time_open !== "closed" && item.time_close !== "closed");
+                                                    return <div>{getTime.time_open} AM - {getTime.time_close} PM</div>
+                                                })(item)
                                                 }
                                             </p>
                                             <p className="res-rating d-flex gap-2 mb-0">
-                                                <CircleIcon/>
-                                                <p className='mb-0'>{item.rating-Math.floor(item.rating)===0?`${item.rating}.0`:item.rating}</p>        
+                                                <CircleIcon />
+                                                <p className='mb-0'>{item.rating - Math.floor(item.rating) === 0 ? `${item.rating}.0` : item.rating}</p>
                                             </p>
                                         </div>
                                     </div>
-                                    
+
                                 </div>
                                 <div className="res-pic d-flex">
                                     {
-                                        item.images.map((image, index)=>(
-                                            <img src={image}/>
+                                        item.images.map((image, index) => (
+                                            <img src={image} />
                                         ))
-                                    }             
+                                    }
                                 </div>
-                                
                             </div>
-                        ))    
+                            
+
+                        ))
 
                     }
                 </div>
-                <Pagination/>
+                <Pagination />
             </div>
 
         </div>
